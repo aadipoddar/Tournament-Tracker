eeb44fe4d82e87c49e71f61fc1d4df95d3b1e673
Aadi Poddar <aadipoddarmail@gmail.com>
2023-02-04 16:48:46 +0530

    Class Library Creation
    Created a Class Library and defined the properties

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,24 @@
+﻿namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one team in a matchup
+    /// </summary>
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents One Team in the Matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score for this Particular Team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the matchup that this team came
+        /// from as the winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one team in the tournament
+    /// </summary>
+    public class TeamModel
+    {
+        /// <summary>
+        /// List of team members
+        /// </summary>
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+
+        /// <summary>
+        /// The name of the team
+        /// </summary>
+        public string TeamName { get; set; }
+    }
+}
