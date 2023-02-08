a48bd64eeb1120046d63ddc887af4e6ea73e1639
Aadi Poddar <aadipoddarmail@gmail.com>
2023-02-08 22:49:15 +0530

    Create Tournament Form Part 4
    Logic for Creating Rounds of Matchups

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -121,7 +121,9 @@ namespace TrackerUI
             tm.Prizes = selectedPrizes;
             tm.EnteredTeams = selectedTeams;
 
-            // TODO - Wire our matchups
+            // Wire our matchups
+            TournamentLogic.CreateRounds(tm);
+
 
             // Create Tournament Entry
             // Create all of the prizes entries
